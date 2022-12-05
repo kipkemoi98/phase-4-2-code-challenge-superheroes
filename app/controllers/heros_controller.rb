bcb1494b4da6824c0989e12ed7d3529a73889ae7
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-12-05 09:48:49 +0300

    done

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,16 @@
 class HerosController < ApplicationController
-end
+    #protect_from_forgery with: :null_session
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index
+        heroes = Hero.all
+        render json: heroes
+    end
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero, serializer: HerowithpowerSerializer
+    end
+    private
+    def render_not_found_response
+    render json: { error: "Hero not found" }, status: :not_found
+    end
+   end
